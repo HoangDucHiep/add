8189311edfef8dd8b7f89d244f5a51fc6d47d323
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2026-02-05 20:41:45 +0700

    Add godoc comments

--- add.go
+++ add.go
@@ -1,5 +1,8 @@
+// Package add provides a simple function to add two integers.
 package add
 
+// Add returns the sum of two integers.
+// It takes two int parameters and returns their sum as an int.
 func Add(a int, b int) int {
 	return a + b
 }
