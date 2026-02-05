d3a7380d26878132b17547eaf2aa9b2d4f6299ad
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2026-02-05 20:44:33 +0700

    Make Add generic using constraints

--- add.go
+++ add.go
@@ -1,8 +1,14 @@
 // Package add provides a simple function to add two integers.
 package add
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // Add returns the sum of two integers.
 // It takes two int parameters and returns their sum as an int.
-func Add(a int, b int) int {
+func Add[T Number](a T, b T) T {
 	return a + b
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/HoangDucHiep/add
+module github.com/HoangDucHiep/add/v2
 
 go 1.25.5
+
+require golang.org/x/exp v0.0.0-20260112195511-716be5621a96 // indirect
